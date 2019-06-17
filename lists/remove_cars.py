fab1dcb1c3162f5f1728b471531b3db5e5bb427a
irfan87 <nerve2009@yahoo.com>
2019-06-17 13:01:45 +0800

    play around with append

---
+++ lists/remove_cars.py
@@ -0,0 +1,9 @@
+cars = ['proton', 'kia', 'porsche']
+
+remove_car = cars[0]
+cars.remove(remove_car)
+
+message = f"{remove_car.title()} have been removed from your garage because you have many cars"
+
+print(cars)
+print(message)
