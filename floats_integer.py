4efc224c94e187871074e50b9a8d6b3eb9af7a91
irfan87 <nerve2009@yahoo.com>
2019-06-17 10:19:27 +0800

    playing with numbers and floats

---
+++ floats_integer.py
@@ -0,0 +1,8 @@
+float_number_one = 0.1
+float_number_two = 0.2
+regular_number = 2
+
+result_calc = float_number_one + float_number_two
+result_calc_2 = result_calc + regular_number
+
+print(result_calc_2)
---
+++ mix_calculation.py
@@ -0,0 +1,8 @@
+first_number = 2
+second_number = 3
+third_number = 4
+fourth_number = 5
+
+mix_result = (first_number + second_number) / third_number * fourth_number
+
+print(f"The result could be {mix_result}")
