a6a1740cf03e9a2955ba753e5566f8eb961d35c0
irfan87 <nerve2009@yahoo.com>
2019-06-17 10:29:00 +0800

    play around the numbers and make a calculation

---
+++ favorite_number.py
@@ -0,0 +1,3 @@
+favorite_number = 15
+
+print(f"My favorite number is {favorite_number}\n")
---
+++ number_eight.py
@@ -0,0 +1,9 @@
+addition = 5 + 3
+substract = 10 - 2
+multiplication = 4 * 2
+division = 8 / 1
+
+print(f"Addition result: {addition}\n")
+print(f"Substract result {substract}\n")
+print(f"Multiplication result: {multiplication}\n")
+print(f"Division result: {division}\n")
