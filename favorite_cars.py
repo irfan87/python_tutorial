b81ada2495ef8376ee7a7efc9546c9ebcc0433b8
irfan87 <nerve2009@yahoo.com>
2019-06-17 11:19:54 +0800

    play around with greeting and my favorite car list

---
+++ favorite_cars.py
@@ -0,0 +1,7 @@
+# list of my favorite cars
+favorite_car = ['honda', 'nissan', 'lamborghini', 'mercedes']
+
+message = f"I would like to own {favorite_car[0].title()} car."
+
+print(favorite_car)
+print(message)
