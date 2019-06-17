d3642b78808029fe4e683798ed0bdcafff81db88
irfan87 <nerve2009@yahoo.com>
2019-06-17 11:45:16 +0800

    greeting listing

--- greeting_friends.py
+++ greeting_friends.py
@@ -1,4 +1,4 @@
-friends_list = ['Ivy', 'Nik', 'Amin', 'Sol', 'Bihah']
+friends_list = ['Ivy', 'Nik', 'Amin', 'Sol', 'Bihah', 'Ika']
 
 # friend specific list
 friend_one = friends_list[0].title()
@@ -6,6 +6,7 @@ friend_two = friends_list[1].title()
 friend_three = friends_list[2].title()
 friend_four = friends_list[3].title()
 friend_five = friends_list[4].title()
+friend_six = friends_list[5].title()
 
 # greeting lists for specific friend
 greeting_msg_one = f"Hi {friend_one}!\n"
@@ -13,6 +14,7 @@ greeting_msg_two = f"Hi {friend_two}!\n"
 greeting_msg_three = f"Hi {friend_three}!\n"
 greeting_msg_four = f"Hi {friend_four}!\n"
 greeting_msg_five = f"Hi {friend_five}!\n"
+greeting_msg_six = f"Hi {friend_six}!\n"
 
 print(friends_list)
 print(greeting_msg_one)
@@ -20,3 +22,4 @@ print(greeting_msg_two)
 print(greeting_msg_three)
 print(greeting_msg_four)
 print(greeting_msg_five)
+print(greeting_msg_six)
