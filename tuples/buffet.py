f37cdcdff0cacdf27ad64877dc9c51873da5fb78
irfan87 <nerve2009@yahoo.com>
2019-06-25 18:47:24 +0800

    playing around with tuples

---
+++ tuples/buffet.py
@@ -0,0 +1,14 @@
+# store buffet in tuple
+our_foods = ('nasi goreng', 'mee goreng', 'bihun goreng', 'mix grill', 'roti canai')
+
+print("\nBelow is the buffet that we are currently offer:")
+for our_food in list(our_foods):
+    print(our_food.title())
+
+# our_foods[0] = 'nasi goreng kampung'
+# print(our_food[0])
+
+food_changes = ('nasi goreng ayam', 'mee soto')
+print("\nWe have make a few changes of buffet as below:")
+for food_change in list(food_changes):
+    print(food_change)
