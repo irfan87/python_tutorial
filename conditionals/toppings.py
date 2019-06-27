5e6788a9ae76508729a6882e80a58a6734223d6d
irfan87 <nerve2009@yahoo.com>
2019-06-27 15:24:32 +0800

    update pizzeria code

--- conditionals/toppings.py
+++ conditionals/toppings.py
@@ -1,21 +1,22 @@
 # make this app efficiently
 
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
 requested_toppings = []
 
 requested_toppings.append('mushrooms')
 requested_toppings.append('extra cheese')
-requested_toppings.append('pepperoni')
-requested_toppings.append('green peppers')
-requested_toppings.append('anchovies')
+requested_toppings.append('french fries')
 
 if requested_toppings:
     for requested_topping in requested_toppings:
-        if requested_topping == 'green peppers':
+        if requested_topping in available_toppings:
+            print(f"Adding {requested_topping.title()}")
+        elif requested_topping == 'green peppers':
             print("Sorry, green peppers are out of stocks")
         elif requested_topping == 'anchovies':
             print("Sorry, anchovies is out of stocks")
         else:
-            print(f"Adding {requested_topping.title()}")
+            print(f"Sorry, we don't have {requested_topping.title()}")
 
     print("\nYour pizza is ready")
 else:
