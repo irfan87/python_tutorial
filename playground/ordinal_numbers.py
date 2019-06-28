46a01361f49bda90a81b3775bed2ecdff17e5bee
irfan87 <nerve2009@yahoo.com>
2019-06-28 15:11:24 +0800

    playaround with the ordinal numbers

---
+++ playground/ordinal_numbers.py
@@ -0,0 +1,12 @@
+# create a list of numbers
+numbers_list = list(range(1, 10))
+
+for number in numbers_list:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{str(number)}th")
