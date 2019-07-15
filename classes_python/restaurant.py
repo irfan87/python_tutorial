8e3ec5c8b6afb75528193ac611e4cc216f746517
irfan87 <nerve2009@yahoo.com>
2019-07-15 10:52:58 +0800

    do some cool stuff with classes and instances

--- classes_python/restaurant.py
+++ classes_python/restaurant.py
@@ -2,12 +2,19 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name.title()
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"\n{self.restaurant_name.title()} is serving {self.cuisine_type.title()}")
 
     def open_restaurant(self):
         print(f"\n{self.restaurant_name.title()} is opening now!\n")
+
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+    def increment_number_served(self, additional_served):
+        self.number_served += additional_served
 
 my_restaurant = Restaurant('irfan restaurant', 'kelantan cuisine')
 ivy_restaurant = Restaurant('ivy restaurant', 'chinese cuisine')
@@ -17,6 +24,16 @@ print(my_restaurant.cuisine_type)
 
 my_restaurant.describe_restaurant()
 my_restaurant.open_restaurant()
+
+print(f"\nNumber served: {str(my_restaurant.number_served)}")
+my_restaurant.number_served = 123
+print(f"\nNumber served: {str(my_restaurant.number_served)}\n")
+
+my_restaurant.set_number_served(1506)
+print(f"\nNumber served: {str(my_restaurant.number_served)}\n")
+
+my_restaurant.increment_number_served(20)
+print(f"\nNumber served: {str(my_restaurant.number_served)}\n")
 
 print(ivy_restaurant.restaurant_name)
 ivy_restaurant.describe_restaurant()
--- classes_python/user.py
+++ classes_python/user.py
@@ -4,6 +4,7 @@ class User:
         self.last_name = last_name
         self.age = age
         self.job_title = job_title
+        self.login_attempts = 0
 
     def describe_user(self):
         print(f"\nFullname: {self.first_name.title()} {self.last_name.title()}")
@@ -13,6 +14,12 @@ class User:
     def greet_user(self):
         print(f"Assalamualaikum, {self.first_name.title()}")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 user = User('ahmad irfan', 'mohammad shukri', 32, 'programmer')
 print(user.first_name)
 print(user.last_name)
@@ -21,3 +28,14 @@ print(user.job_title)
 
 user.describe_user()
 user.greet_user()
+
+# increment login attempt
+print("Making login attempt: ")
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+print(f"You have attempt to login in {user.login_attempts} times")
+
+print("\nReset login attempt")
+user.reset_login_attempts()
+print(f"Reset to {user.login_attempts}")
