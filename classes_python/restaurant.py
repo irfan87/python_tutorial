fc30fa359f0806c9a09bd26a06c401ddac060799
irfan87 <nerve2009@yahoo.com>
2019-07-15 22:13:30 +0800

    modified certain file for class inheritance

--- classes_python/restaurant.py
+++ classes_python/restaurant.py
@@ -1,3 +1,4 @@
+# parent class
 class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name.title()
@@ -15,6 +16,16 @@ class Restaurant:
 
     def increment_number_served(self, additional_served):
         self.number_served += additional_served
+
+# child class (IceCreamStand) inherit from the parent class (Restaurant)
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type='ice_cream'):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = []
+
+    def show_ice_cream_flavor(self):
+        for flavor in self.flavors:
+            print(f"{flavor.title()}")
 
 my_restaurant = Restaurant('irfan restaurant', 'kelantan cuisine')
 ivy_restaurant = Restaurant('ivy restaurant', 'chinese cuisine')
@@ -34,6 +45,13 @@ print(f"\nNumber served: {str(my_restaurant.number_served)}\n")
 
 my_restaurant.increment_number_served(20)
 print(f"\nNumber served: {str(my_restaurant.number_served)}\n")
+
+magnum_stand = IceCreamStand('magnum ice cream stand')
+magnum_stand.flavors = ['chocolate', 'vanilla', 'black chocolate']
+
+magnum_stand.describe_restaurant()
+magnum_stand.show_ice_cream_flavor()
+print("\n")
 
 print(ivy_restaurant.restaurant_name)
 ivy_restaurant.describe_restaurant()
