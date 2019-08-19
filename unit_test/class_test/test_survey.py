b96c9a9039c8cd955955a8b7836b1d9fbcb25863
irfan87 <nerve2009@yahoo.com>
2019-08-19 10:56:01 +0800

    test classes

---
+++ unit_test/class_test/test_survey.py
@@ -0,0 +1,26 @@
+# test for AnonymousTest.py
+
+import unittest
+from survey import AnonymousSurvey
+
+class TestAnonymousSurvey(unittest.TestCase):
+    def setUp(self):
+        # create a survey and a set of responses for use in all test methods
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Mandarin', 'Japanese']
+
+    def test_store_single_response(self):
+        self.responses = ['English', 'Mandarin', 'Japanese']
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_responses(self):
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
+if __name__ == '__main__':
+    unittest.main()
